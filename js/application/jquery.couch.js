36b4eafd8f410d9be8c016a9867b33a34c951340
Patrick de Lanauze <patrick.delanauze@gmail.com>
2011-12-13 00:59:08 -0500

    Fixing bad reference to jquery dependency

--- js/application/jquery.couch.js
+++ js/application/jquery.couch.js
@@ -10,7 +10,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-define(['./jquery.min'], function($){
+define(['jquery'], function($){
 
 (function($) {
   $.couch = $.couch || {};
