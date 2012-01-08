78f27f15d2e0ba501881331d95befe807945432d
Patrick de Lanauze <patrick.delanauze@gmail.com>
2012-01-08 01:58:49 -0500

    Adding a utility module

---
+++ js/application/utility.js
@@ -0,0 +1,23 @@
+define('utility', ['jQuery'], function ($) {
+  var Utility = {
+    DOM:{
+      serializeObject:function (el) {
+        var o = {};
+        var a = $(el).serializeArray();
+        $.each(a, function () {
+          if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+              o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+          } else {
+            o[this.name] = this.value || '';
+          }
+        });
+        return o;
+      }
+    }
+  };
+
+  return Utility;
+});
