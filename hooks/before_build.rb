9ad898767685ac709e824ad9f3c9f4f69cb479aa
Patrick de Lanauze <patrick.delanauze@gmail.com>
2012-04-25 00:32:44 -0400

    Need to fix the r.js optimizer command

--- hooks/before_build.rb
+++ hooks/before_build.rb
@@ -1,4 +1,5 @@
 # any pre build scripting or plugins should go here
 # plugin 'coffeescript'
 # plugin 'jim'
+# TODO Fix this next line
 # `r.js -o name=main out=js/main.all.min.js baseUrl=./js/ paths.jquery=application/jquery.min paths.underscore=application/underscore paths.backbone=application/backbone`
