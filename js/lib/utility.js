ea68614d8bdb13940be635858e3f4c5cd2a5eba4
Patrick de Lanauze <patrick.delanauze@gmail.com>
2012-05-13 02:55:13 -0400

    Adding method to build form structure from a json-schema

--- js/lib/utility.js
+++ js/lib/utility.js
@@ -5,8 +5,8 @@ define(['jquery', 'underscore'], function ($, _) {
     DOM:{},
     String:{},
     Templates:{},
-    File: {},
-    Image: {}
+    File:{},
+    Image:{}
   };
 
   Utility.DOM.serializeObject = function (el) {
@@ -26,70 +26,70 @@ define(['jquery', 'underscore'], function ($, _) {
   };
 
   Utility.Templates.TABLE_STRUCTURE = '<thead>' +
-          '<tr>' +
-          '<% _.each(columns, function(c){ %>' +
-          '<th class="<%= c.value %>"><%= c.name %></th>' +
-          '<% }); %>' +
-          '</tr>' +
-          '<tr>' +
-          '<td colspan="<%= columns.length + 1%>"><a style="display: none;" class="btn btn-large show-less" href="<%= showLess.href || \"#\"%>"><%= showLess.label || "Show less" %></a></td>' +
-          '</tr>' +
-          '</thead>' +
-          '<tbody></tbody>' +
-          '<tfoot><tr>' +
-          '<td colspan="<%= columns.length + 1%>"><a style="display: none;" class="btn btn-large show-more" href="<%= showMore.href || \"#\"%>"><%= showMore.label || "Show more" %></a></td>' +
-          '</tr></tfoot>';
+      '<tr>' +
+      '<% _.each(columns, function(c){ %>' +
+      '<th class="<%= c.value %>"><%= c.name %></th>' +
+      '<% }); %>' +
+      '</tr>' +
+      '<tr>' +
+      '<td colspan="<%= columns.length + 1%>"><a style="display: none;" class="btn btn-large show-less" href="<%= showLess.href || \"#\"%>"><%= showLess.label || "Show less" %></a></td>' +
+      '</tr>' +
+      '</thead>' +
+      '<tbody></tbody>' +
+      '<tfoot><tr>' +
+      '<td colspan="<%= columns.length + 1%>"><a style="display: none;" class="btn btn-large show-more" href="<%= showMore.href || \"#\"%>"><%= showMore.label || "Show more" %></a></td>' +
+      '</tr></tfoot>';
 
   Utility.Templates.FORM_STRUCTURE = '<form action="<%= action %>" method="<%= method %>" class="<%= formClass %>"><fieldset>' +
-          '<% if(legend){ %>' +
-          '<legend><%= legend %></legend>' +
-          '<% } %>' +
-          '<% _.each(fields, function(field){ %>' +
-          '<% if(field.type != "hidden"){ %>' +
-          '<div class="control-group <%= field.outerClass %>">' +
-          '<label class="control-label" for="<%= field.idPrefix %>-<%= recordId %>-<%= field.name%>"><%= field.humanName %></label>' +
-          '<div class="controls <%= field.inputOuterClass %>">' +
+      '<% if(legend){ %>' +
+      '<legend><%= legend %></legend>' +
+      '<% } %>' +
+      '<% _.each(fields, function(field){ %>' +
+      '<% if(field.type != "hidden"){ %>' +
+      '<div class="control-group <%= field.outerClass %>">' +
+      '<label class="control-label" for="<%= field.idPrefix %>-<%= recordId %>-<%= field.name%>"><%= field.humanName %></label>' +
+      '<div class="controls <%= field.inputOuterClass %>">' +
 
-          '<% if(field.type == "textarea") { %>' +
-          '<textarea class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" rows="<%= field.rows %>" name="<%= field.name %>" <% if(field.disabled){ %>disabled="disabled" <% } %>><%= field.value %></textarea>' +
-          '<% } else if (field.type == "select" || field.type == "select-multiple"){ %>' +
-          '<select class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" name="<%= field.name %>" <%= (field.type == \'select-multiple\') ? \'multiple="multiple"\' : \'\'%>> ' +
-          '<% _.each(field.options, function(option){ %>' +
-          '<% if (option.optgroup){ %>' +
-          '<optgroup label="<%= option.label %>">' +
-          '<% _.each(option.options, function(subOption){ %>' +
-          '<option value="<%= subOption.value %>" <%= (subOption.value == field.value || (_.isArray(field.value) && _.include(field.value, subOption.value))) ? \'selected="selected" \' : \'\' %>><%= subOption.label %></option> ' +
-          '<% }); %>' +
-          '</optgroup>' +
-          '<% } else { %>' +
-          '<option value="<%= option.value %>" <%= (option.value == field.value || (_.isArray(field.value) && _.include(field.value, option.value))) ? \'selected="selected" \' : \'\' %>><%= option.label %></option> ' +
-          '<% } %>' +
-          '<% }); %>' +
-          '</select>' +
-          '<% } else if (field.type == "checkbox"){ %>' +
-          '<input class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" checked="<%= (field.value ? \'checked\' : \'\') %>" name="<%= field.name %>" type="<%= field.type %>" <% if(field.disabled){ %>disabled="disabled" <% } %> /> ' +
-          '<% } else { %>' +
-          '<input class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" value="<%= field.value %>" name="<%= field.name %>" size="<%= field.size %>" type="<%= field.type %>" <% if(field.disabled){ %>disabled="disabled" <% } %> <% if (field.max) { %> max="<%= field.max %>" <% } %> <% if (field.min) { %> min="<%= field.min %>" <% } %> <% if (field.step) { %> step="<%= field.step %>" <% } %> /> ' +
-          '<% } %>' +
+      '<% if(field.type == "textarea") { %>' +
+      '<textarea class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" rows="<%= field.rows %>" name="<%= field.name %>" <% if(field.disabled){ %>disabled="disabled" <% } %>><%= field.value %></textarea>' +
+      '<% } else if (field.type == "select" || field.type == "select-multiple"){ %>' +
+      '<select class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" name="<%= field.name %>" <%= (field.type == \'select-multiple\') ? \'multiple="multiple"\' : \'\'%>> ' +
+      '<% _.each(field.options, function(option){ %>' +
+      '<% if (option.optgroup){ %>' +
+      '<optgroup label="<%= option.label %>">' +
+      '<% _.each(option.options, function(subOption){ %>' +
+      '<option value="<%= subOption.value %>" <%= (subOption.value == field.value || (_.isArray(field.value) && _.include(field.value, subOption.value))) ? \'selected="selected" \' : \'\' %>><%= subOption.label %></option> ' +
+      '<% }); %>' +
+      '</optgroup>' +
+      '<% } else { %>' +
+      '<option value="<%= option.value %>" <%= (option.value == field.value || (_.isArray(field.value) && _.include(field.value, option.value))) ? \'selected="selected" \' : \'\' %>><%= option.label %></option> ' +
+      '<% } %>' +
+      '<% }); %>' +
+      '</select>' +
+      '<% } else if (field.type == "checkbox"){ %>' +
+      '<input class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" checked="<%= (field.value ? \'checked\' : \'\') %>" name="<%= field.name %>" type="<%= field.type %>" <% if(field.disabled){ %>disabled="disabled" <% } %> /> ' +
+      '<% } else { %>' +
+      '<input class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" value="<%= field.value %>" name="<%= field.name %>" size="<%= field.size %>" type="<%= field.type %>" <% if(field.disabled){ %>disabled="disabled" <% } %> <% if (field.max) { %> max="<%= field.max %>" <% } %> <% if (field.min) { %> min="<%= field.min %>" <% } %> <% if (field.step) { %> step="<%= field.step %>" <% } %> /> ' +
+      '<% } %>' +
 
-          '<span class="help-inline"></span>' +
+      '<span class="help-inline"></span>' +
 
-          '</div>' +
-          '</div>' +
-          '<% } else { %>' +
-          '<input class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" value="<%= field.value %>" name="<%= field.name %>" size="<%= field.size %>" type="<%= field.type %>" /> ' +
-          '<% } %>' + // End the if type=='hidden'
-          '<% }); %>' +
+      '</div>' +
+      '</div>' +
+      '<% } else { %>' +
+      '<input class="<%= field.inputClass %>" id="<%= field.idPrefix %>-<%= recordId %>-<%= field.name %>" value="<%= field.value %>" name="<%= field.name %>" size="<%= field.size %>" type="<%= field.type %>" /> ' +
+      '<% } %>' + // End the if type=='hidden'
+      '<% }); %>' +
 
-          '<div class="form-actions">' +
-          '<% _.each(buttons, function(button){ %>' +
-          '<% if(button.type == "link"){ %>' +
-          '<a class="btn <%= button.class %>" href="<%= button.href %>"><%= button.humanName %></a>&nbsp;' +
-          '<% } else { %>' +
-          '<button class="btn <%= button.class %>" type="<%= button.type %>"><%= button.humanName %></button>&nbsp;' +
-          '<% }}); %>' +
-          '</div>' +
-          '</fieldset></form>';
+      '<div class="form-actions">' +
+      '<% _.each(buttons, function(button){ %>' +
+      '<% if(button.type == "link"){ %>' +
+      '<a class="btn <%= button.class %>" href="<%= button.href %>"><%= button.humanName %></a>&nbsp;' +
+      '<% } else { %>' +
+      '<button class="btn <%= button.class %>" type="<%= button.type %>"><%= button.humanName %></button>&nbsp;' +
+      '<% }}); %>' +
+      '</div>' +
+      '</fieldset></form>';
 
   /**
    * Renders an underscore.js template with the provided structure data.
@@ -108,9 +108,9 @@ define(['jquery', 'underscore'], function ($, _) {
     }
 
     _.defaults(structureData, {
-      showMore: false,
-      showLess: false,
-      columns: []
+      showMore:false,
+      showLess:false,
+      columns:[]
     });
 
     return _.template(template, structureData);
@@ -135,7 +135,7 @@ define(['jquery', 'underscore'], function ($, _) {
    * - time
    * - url
    * - date
-   * - datetime
+   * - datetime (|| date-time)
    * - datetime-local
    * - time
    * - month
@@ -204,12 +204,12 @@ define(['jquery', 'underscore'], function ($, _) {
       legend:false,
       fields:[],
       buttons:[],
-      idPrefix: now
+      idPrefix:now
     });
 
     _.each(formStructure.fields, function (field) {
       _.defaults(field, {
-        idPrefix: 'field-' + formStructure.idPrefix,
+        idPrefix:'field-' + formStructure.idPrefix,
         outerClass:'',
         name:'',
         humanName:'',
@@ -217,14 +217,14 @@ define(['jquery', 'underscore'], function ($, _) {
         value:'',
         inputClass:'',
         type:'text',
-        size: ''
+        size:''
       });
 
       if (field.type == 'text' && !field.size) {
         field.size = 30;
-      } else if (field.type == 'select' || field.type == 'select-multiple'){
+      } else if (field.type == 'select' || field.type == 'select-multiple') {
         _.defaults(field, {
-          options: []
+          options:[]
         });
       } else if (field.type.match(/^date.*/) || field.type == 'month' || field.type == 'week' || field.type == 'time') {
         _.defaults(field, {
@@ -288,7 +288,7 @@ define(['jquery', 'underscore'], function ($, _) {
       var value = jsonModel[key];
       var type = false;
 
-      if (key == 'id' || key == '_id'){
+      if (key == 'id' || key == '_id') {
         type = 'hidden';
       } else if (_.isBoolean(value)) {
         type = 'checkbox';
@@ -309,7 +309,7 @@ define(['jquery', 'underscore'], function ($, _) {
           inputOuterClass:'',
           inputClass:'input-xlarge',
           value:value,
-          type:type
+          type:type,
         });
       } else {
         console.log('buildFormStructureFromModel: does not know how to handle type of "' + key + '" in jsonModel:', jsonModel);
@@ -326,9 +326,137 @@ define(['jquery', 'underscore'], function ($, _) {
 
   };
 
+  Utility.Templates.buildFormStructureFromSchema = function (jsonModel, schema, formStructure, options) {
+    var recordId = jsonModel._id || jsonModel.id;
+
+    if (!options) {
+      options = {};
+    }
+    var humanName = options.humanName || Utility.String.uncapitalize(schema.name) || 'document';
+    _.defaults(options, {
+      humanName: humanName,
+      capitalHumanName: Utility.String.capitalize(humanName)
+    });
+
+    if (!formStructure) {
+      formStructure = {};
+    }
+    _.defaults(formStructure, {
+      action:'#',
+      method:'POST',
+      recordId:recordId,
+      legend:(recordId ? ('Edit ' + options.humanName + ' ' + recordId) : 'New ' + options.humanName),
+      fields:[],
+      buttons:[]
+    });
+
+    var processProperty = function(key, prop, value) {
+      var type = false;
+
+      switch (prop.format) {
+        case 'email':
+          type = 'email';
+          break;
+        case 'phone':
+          type = 'tel';
+          break;
+        case 'uri':
+          type = 'uri';
+          break;
+        case 'date-time':
+          type = 'datetime';
+          break;
+        case 'date':
+          type = 'date';
+          break;
+        case 'time':
+          type = 'time';
+          break;
+        case 'ip-address':
+          type = 'uri';
+          break;
+        case 'ipv6':
+          type = 'uri';
+          break;
+        case 'host-name':
+          type = 'uri';
+          break;
+      }
+
+      if (!type) {
+        if (prop.type === 'string') {
+          if (prop.maxLength > 150) {
+            type = 'textarea';
+          } else {
+            type = 'text';
+          }
+        } else if (prop.type === 'number' || prop.type === 'integer') {
+          type = 'number';
+        } else if (prop.type === 'boolean') {
+          type = 'checkbox';
+        }
+      }
+
+      var min = prop.minimum || prop.minLength;
+      var max = prop.maximum || prop.maxLength;
+      if (typeof min === 'number' && prop.exclusiveMinimum) {
+        min++;
+      }
+      if (typeof max === 'number' && prop.exclusiveMaximum) {
+        max--;
+      }
+
+      if (type) {
+        formStructure.fields.push({
+          idPrefix:options.humanName,
+          name:key,
+          humanName:(key.charAt(0).toUpperCase() + key.substring(1).replace(/_/g, ' ')),
+          outerClass:'',
+          inputOuterClass:'',
+          inputClass:'input-xlarge',
+          value:value,
+          type:type,
+          min:min,
+          max:max
+        });
+      } else {
+        console.log('buildFormStructureFromSchema: does not know how to handle type of "' + key + '" of schema:', schema);
+      }
+    }
+
+    for(var key in schema.properties) {
+      var prop = schema.properties[key];
+      var value = jsonModel[key];
+      processProperty(key, prop, value);
+    }
+
+    for(var key in schema.additionalProperties){
+      var prop = schema.additionalProperties[key];
+      var value = jsonModel[key];
+      processProperty(key, prop, value);
+    }
+
+    // Now add some buttons
+    if (formStructure.buttons.length == 0) {
+      formStructure.buttons.push({'class':'btn-primary', type:'submit', humanName:'Submit'});
+      formStructure.buttons.push({type:'reset', humanName:'Reset'});
+    }
+
+    return formStructure;
+
+  }
+
   Utility.File.getBase64FromDataURL = function (dataUrl) {
     return dataUrl.match(/base64,(.*)$/)[1];
   };
+
+  Utility.String.capitalize = function(str){
+    return str.charAt(0).toUpperCase() + str.substring(1);
+  };
+
+  Utility.String.uncapitalize = function(str){
+    return str.charAt(0).toLowerCase() + str.substring(1);
+  }
 
   return Utility;
 });
