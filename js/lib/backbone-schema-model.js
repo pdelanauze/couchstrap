38b298bb58b24a6edc6050745b515195d6bae1d4
Patrick de Lanauze <patrick.delanauze@gmail.com>
2012-05-13 02:55:33 -0400

    Simple backbone schema model that uses the json-schema validator recently implemented

---
+++ js/lib/backbone-schema-model.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pat
+ * Date: 12-05-10
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+define(['underscore', 'backbone', 'lib/simple-json-schema'], function (_, Backbone, SimpleJsonSchema) {
+
+  var BackboneSchemaModel = Backbone.Model.extend({
+    schema: false,
+    validate: function(attributes){
+      var report = SimpleJsonSchema.validate(attributes, this.schema, true);
+      if (!report.isValid){
+        return report.errors;
+      }
+    }
+  });
+
+  return BackboneSchemaModel;
+});
