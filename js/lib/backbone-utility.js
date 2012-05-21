202f810890ac43a1ac13e33f17b5be7ee5408fcd
Patrick de Lanauze <patrick.delanauze@gmail.com>
2012-05-21 15:38:03 -0400

    Separated file attachment functionality into another view, extending from the ModelEditView utility class

--- js/lib/backbone-utility.js
+++ js/lib/backbone-utility.js
@@ -214,7 +214,6 @@ define(['jquery', 'underscore', 'backbone', 'modelbinder', 'lib/utility'], funct
     pluralModelName:'',
     formStructure:{},
     events:{},
-    handleFileAttachments:true,
     initialize:function (options) {
 
       _.extend(this, options);
@@ -225,42 +224,15 @@ define(['jquery', 'underscore', 'backbone', 'modelbinder', 'lib/utility'], funct
         'reset form':'doReset',
         'click .btn.cancel':'doCancel',
         'click .btn.delete':'doDelete',
-        'click .btn.delete-attachment':'doDeleteAttachment'
       });
 
-      var ctx = this;
-
-      if (this.handleFileAttachments) {
-        var cancelDefaults = function (e) {
-          e.stopPropagation();
-          e.preventDefault();
-        };
-
-        this.el.addEventListener('dragover', function (e) {
-          cancelDefaults(e);
-          ctx.dragOver(e);
-        }, false);
-        this.el.addEventListener('dragleave', function (e) {
-          cancelDefaults(e);
-          ctx.dragLeave(e);
-        }, false);
-        this.el.addEventListener('drop', function (e) {
-          cancelDefaults(e);
-          ctx.dragDrop(e);
-        }, false);
-      }
-
-      _.bindAll(this, 'render', 'renderForm', 'renderAttachments',
+      _.bindAll(this, 'render', 'renderForm',
               'doSave', 'doReset', 'doCancel', 'close', 'modelError',
-              'modelChanged', 'doDelete', 'doDeleteAttachment',
-              'dragOver', 'dragLeave', 'dragDrop');
+              'modelChanged', 'doDelete');
 
       this.model.bind('remove', this.close);
       this.model.bind('change', this.modelChanged);
       this.model.bind('error', this.modelError);
-      this.model.bind('change:_attachments', this.renderAttachments);
-
-      this.dragLeave = _.debounce(this.dragLeave, 1200);
 
       this.delegateEvents();
 
@@ -307,10 +279,6 @@ define(['jquery', 'underscore', 'backbone', 'modelbinder', 'lib/utility'], funct
         $(this.el).empty().html(this.template({model:this.model.toJSON()}));
       }
 
-      if (this.handleFileAttachments) {
-        this.renderAttachments();
-      }
-
       if (!this.modelBinder){
         this.modelBinder = new BackboneModelBinder();
       }
@@ -323,57 +291,6 @@ define(['jquery', 'underscore', 'backbone', 'modelbinder', 'lib/utility'], funct
       }
 
       this.modelBinder.bind(this.model, this.el, this.bindings);
-    },
-    renderAttachments:function () {
-      var m = this.model;
-
-      if (this.$('.file-attachment-handler-overlay').length === 0) {
-        // Add the overlay item onto the DOM
-        $(this.el).addClass('handle-file-attachments').append($('<div class="file-attachment-handler-overlay modal-backdrop"><h2>Drop me!</h2></div>'));
-      }
-
-      var attachments = m.get('_attachments');
-      var attachmentViews = [];
-      var attachmentViewTemplate = _.template('<p class="model-attachment-view" style="text-align: right;" data-id="<%= id %>">' +
-              '<span class="pull-left">' +
-              '<% if (url){ %>' +
-              '<a target="blank" href="<%= url %>"><%= id %></a>' +
-              '<% } else { %>' +
-              '<%= id %>' +
-              '<% } %>' +
-              '</span>' +
-              '<button type="button" class="btn btn-danger delete-attachment">Delete</button>' +
-              '</p>')
-      _.each(attachments, function (v, k) {
-        attachmentViews.push(attachmentViewTemplate({
-          id:k,
-          url:v.digest ? ('/' + Backbone.couch_connector.config.db_name + '/' + m.get('_id') + '/' + k) : false,
-          attachment:v
-        }));
-      });
-
-      if (attachmentViews.length > 0) {
-        var attachmentsListContainer = this.$('.model-attachments-view');
-        if (attachmentsListContainer.length === 0) {
-          var attachmentsContainerTemplate = _.template('<div class="control-group attachments-control-group">' +
-                  '<label class="control-label">Attachments</label> ' +
-                  '<div class="controls"><div class="model-attachments-view"></div></div> ' +
-                  '</div>')();
-
-          attachmentsListContainer = $(attachmentsContainerTemplate).
-                  insertBefore(this.$('form:first .form-actions')).
-                  find('.model-attachments-view');
-
-        } else {
-          attachmentsListContainer.empty();
-        }
-
-        attachmentsListContainer.append(attachmentViews.join(''));
-      } else {
-        this.$('.attachments-control-group').remove();
-      }
-
-      return this;
     },
     render:function () {
       return this;
@@ -450,17 +367,115 @@ define(['jquery', 'underscore', 'backbone', 'modelbinder', 'lib/utility'], funct
       }
       return false;
     },
+    close:function () {
+      this.remove();
+      this.unbind();
+      this.modelBinder.unbind(this);
+    }
+  });
+
+  BackboneUtility.Views.AttachmentModelEditView = BackboneUtility.Views.ModelEditView.extend({
+    initialize:function (options) {
+
+      BackboneUtility.Views.ModelEditView.prototype.initialize.call(this, options);
+
+      _.defaults(this.events, {
+        'click .btn.delete-attachment':'doDeleteAttachment'
+      });
+
+      var ctx = this;
+
+      // Attach drag events on the element
+      var cancelDefaults = function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+      };
+
+      this.el.addEventListener('dragover', function (e) {
+        cancelDefaults(e);
+        ctx.dragOver(e);
+      }, false);
+
+      this.el.addEventListener('dragleave', function (e) {
+        cancelDefaults(e);
+        ctx.dragLeave(e);
+      }, false);
+
+      this.el.addEventListener('drop', function (e) {
+        cancelDefaults(e);
+        ctx.dragDrop(e);
+      }, false);
+
+      _.bindAll(this, 'renderAttachments', 'doDeleteAttachment', 'dragOver', 'dragLeave', 'dragDrop');
+      this.model.bind('change:_attachments', this.renderAttachments);
+
+      this.dragLeave = _.debounce(this.dragLeave, 1200);
+
+    },
+
+    renderForm:function () {
+      BackboneUtility.Views.ModelEditView.prototype.renderForm.call(this);
+      this.renderAttachments();
+    },
+
+    renderAttachments:function () {
+      var m = this.model;
+
+      if (this.$('.file-attachment-handler-overlay').length === 0) {
+        // Add the overlay item onto the DOM
+        $(this.el).addClass('handle-file-attachments').append($('<div class="file-attachment-handler-overlay modal-backdrop"><h2>Drop it!</h2></div>'));
+      }
+
+      var attachments = m.get('_attachments');
+      var attachmentViews = [];
+      var attachmentViewTemplate = _.template('<p class="model-attachment-view" style="text-align: right;" data-id="<%= id %>">' +
+          '<span class="pull-left">' +
+          '<% if (url){ %>' +
+          '<a target="blank" href="<%= url %>"><%= id %></a>' +
+          '<% } else { %>' +
+          '<%= id %>' +
+          '<% } %>' +
+          '</span>' +
+          '<button type="button" class="btn btn-danger delete-attachment">Delete</button>' +
+          '</p>')
+      _.each(attachments, function (v, k) {
+        attachmentViews.push(attachmentViewTemplate({
+          id:k,
+          url:v.digest ? ('/' + Backbone.couch.options.database + '/' + m.get('_id') + '/' + k) : false,
+          attachment:v
+        }));
+      });
+
+      if (attachmentViews.length > 0) {
+        var attachmentsListContainer = this.$('.model-attachments-view');
+        if (attachmentsListContainer.length === 0) {
+          var attachmentsContainerTemplate = _.template('<div class="control-group attachments-control-group">' +
+              '<label class="control-label">Attachments</label> ' +
+              '<div class="controls"><div class="model-attachments-view"></div></div> ' +
+              '</div>')();
+
+          attachmentsListContainer = $(attachmentsContainerTemplate).
+              insertBefore(this.$('form:first .form-actions')).
+              find('.model-attachments-view');
+
+        } else {
+          attachmentsListContainer.empty();
+        }
+
+        attachmentsListContainer.append(attachmentViews.join(''));
+      } else {
+        this.$('.attachments-control-group').remove();
+      }
+
+      return this;
+    },
     doDeleteAttachment:function (e) {
       var id = $(e.target).closest('.model-attachment-view').data('id');
       delete this.model.get('_attachments')[id];
       this.model.trigger('change:_attachments');
       return false;
     },
-    close:function () {
-      this.remove();
-      this.unbind();
-      this.modelBinder.unbind(this);
-    },
+
     dragOver:function (e) {
       $(this.el).addClass('drag-over');
       this.dragLeave(e);
